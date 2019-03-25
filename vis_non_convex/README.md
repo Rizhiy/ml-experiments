f018925ee32c480ad450f99e0d1c2e9827536f07
Artem Vasenin <vasart169@gmail.com>
2019-03-25 04:31:54 +0300

    [vis_non_convex] Improve plots
    fix: Fix bug in order calculation
    feat: Add color to scatter plots
    feat: Add loss
    refactor: Move text to README.md

--- vis_non_convex/README.md
+++ vis_non_convex/README.md
@@ -10,4 +10,9 @@ Results which are produced using `plot_train.py`, can be seen below.
 
 ![Results](Paths.png)
 
+As can be seen on the plots, some parameters have quite a lot of displacement during training.
+The direction of movement for these parameters also changes quite a few times during training,
+even when using momentum.
+As a result parameters can pass through some values multiple times during training.
+
 Running `plot_train.py` will also show the same figure with 3d plot being interactive.
--- vis_non_convex/plot_train.py
+++ vis_non_convex/plot_train.py
@@ -6,6 +6,7 @@ from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from matplotlib.colors import Normalize
 from mpl_toolkits.mplot3d import Axes3D
 from torch import nn
 from torch.optim import SGD
@@ -14,9 +15,8 @@ from torchvision.datasets import CIFAR10
 from torchvision.models.resnet import resnet18
 from torchvision.transforms import Compose, Normalize, ToTensor
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+sample_size = 200
 print_freq = 100
-base_lr = 0.01
 
 cache_path = Path('cache.pkl')
 
@@ -31,32 +31,29 @@ def get_param_subset(model: nn.Module, indices: List[int]) -> np.ndarray:
 def train():
     if cache_path.exists():
         with cache_path.open('rb') as f:
-            param_data = pickle.load(f)
+            param_data, loss_hist = pickle.load(f)
     else:
         model = resnet18(num_classes=10)
         model = nn.DataParallel(model.cuda())
 
-        lr = base_lr
         transform = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         dataset = CIFAR10('./data', train=True, download=True, transform=transform)
         loader = DataLoader(dataset, batch_size=256, shuffle=True, num_workers=4)
         dataiter = iter(loader)
 
         criterion = nn.CrossEntropyLoss()
-        optimizer = SGD(model.parameters(), lr, momentum=0.9, weight_decay=0.0005)
+        optimizer = SGD(model.parameters(), 0.01, momentum=0.9, weight_decay=0.0005)
 
         num_params = sum([p.numel() for p in model.parameters()])
         indices = list(range(num_params))
         random.shuffle(indices)
-        chosen_indices = indices[:100]
+        chosen_indices = indices[:sample_size]
         param_data = []
 
         total_loss = 0
         model.train()
-        for idx in range(6000):
-            if idx in [4000]:
-                lr *= 0.1
-                optimizer = SGD(model.parameters(), lr, momentum=0.9, weight_decay=0.0005)
+        loss_hist = []
+        for idx in range(5000):
             try:
                 inputs, labels = next(dataiter)
             except StopIteration:
@@ -71,7 +68,9 @@ def train():
             loss.backward()
             optimizer.step()
 
-            total_loss += float(loss)
+            loss = float(loss)
+            total_loss += loss
+            loss_hist.append(loss)
 
             param_data.append(get_param_subset(model, chosen_indices))
 
@@ -80,59 +79,59 @@ def train():
                 total_loss = 0
 
         param_data = np.array(param_data)
+        loss_hist = np.array(loss_hist)
 
         with cache_path.open('wb') as f:
-            pickle.dump(param_data, f)
+            pickle.dump((param_data, loss_hist), f)
 
-    swaped = np.swapaxes(param_data, 0, 1)
-    displacements = np.sum(np.abs(swaped[1:] - swaped[:-1]), axis=1)
+    smooth_loss_hist = list(loss_hist[:1])
+    for loss in loss_hist:
+        smooth_loss_hist.append(smooth_loss_hist[-1] * 0.95 + 0.05 * loss)
+
+    colors = np.arange(len(param_data))
+
+    displacements = np.sum(np.abs(param_data[1:] - param_data[:-1]), axis=0)
+
     order = np.argsort(displacements)[::-1]
 
     fig = plt.figure(figsize=(14, 12))
     ax = plt.subplot(2, 2, 1)
+    ax.plot(loss_hist, c='lightblue')
+    ax.plot(smooth_loss_hist, c='blue')
+    ax.set_title("Loss graph")
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Loss")
+    ax.set_yscale('log')
+    ax.set_yticks([0.01, 0.1, 1.0])
+    ax.set_yticklabels(['0.01', '0.1', '1'])
+
+    ax = plt.subplot(2, 2, 3)
     for idx, param_idx in enumerate(order):
         param_hist = param_data[:, param_idx]
         ax.plot(param_hist)
         if idx > 10:
             break
-    ax.set_title("Values of parameters with great movement")
+    ax.set_title("Values of parameters with largest movement")
     ax.set_xlabel("Iteration")
     ax.set_ylabel("Parameter values")
 
     ax = plt.subplot(2, 2, 2)
     xs = param_data[:, order[0]]
     ys = param_data[:, order[1]]
-    ax.scatter(xs, ys, s=1)
+    ax.scatter(xs, ys, s=1, c=colors, cmap='hsv')
     ax.set_title("Path of two params with largest movements in the sample")
     ax.set_xlabel("Param 1")
     ax.set_ylabel("Param 2")
     ax.text(xs[0], ys[0], "Start")
-    ax.text(xs[-1], ys[-1], "End")
 
-    ax = plt.subplot(2, 2, 3, projection=Axes3D.name)
+    ax = plt.subplot(2, 2, 4, projection=Axes3D.name)
     zs = param_data[:, order[2]]
-    ax.scatter(xs, ys, zs, s=1)
+    ax.scatter(xs, ys, zs, s=1, c=colors, cmap='hsv')
     ax.set_title("Path of three params with largest movements in the sample")
     ax.set_xlabel("Param 1")
     ax.set_ylabel("Param 2")
     ax.set_zlabel("Param 3")
     ax.text(xs[0], ys[0], zs[0], "Start")
-    ax.text(xs[-1], ys[-1], zs[-1], "End")
-
-    ax = plt.subplot(2, 2, 4)
-    ax.axis([0, 1, 0, 1])
-    ax.text(0.5, 0.5,
-            "As can be seen on the plots, some parameters\n"
-            "have quite a lot of displacement during training.\n"
-            "\n"
-            "The direction of movement for these parameters\n"
-            "also changes quite a few times during training,\n"
-            "even when using momentum.\n"
-            "\n"
-            "As a result parameters can pass through some\n"
-            "values multiple times during training.",
-            ha='center', va='center', wrap=True, fontsize=16)
-    ax.axis('off')
 
     fig.tight_layout()
     fig.savefig("Paths.png")
